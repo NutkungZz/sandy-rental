0569734bfe679ec85786746e4a0afa12eee44c9c
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-06-27 13:14:58 +0700

    Update rooms.js

--- api/rooms.js
+++ api/rooms.js
@@ -1,33 +1,29 @@
-import { createClient } from '@supabase/supabase-js';
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = "https://lqskrutydtcnnszvxaur.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxxc2tydXR5ZHRjbm5zenZ4YXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NTUzMDgsImV4cCI6MjAzNTAzMTMwOH0.9-LvOBprRoIdjnMpWATC7gIVfYa_RoJsgfNyEZegWbk";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default async function handler(req, res) {
+module.exports = async (req, res) => {
   try {
     if (req.method === 'GET') {
       const { data, error } = await supabase
         .from('rooms')
         .select('*');
-
       if (error) {
         console.error('Error fetching rooms:', error.message);
         throw error;
       }
-
       res.status(200).json(data);
     } else if (req.method === 'POST') {
       const { body } = req;
       const { data, error } = await supabase
         .from('rooms')
         .insert(body);
-
       if (error) {
         console.error('Error inserting room:', error.message);
         throw error;
       }
-
       res.status(201).json(data);
     } else {
       res.setHeader('Allow', ['GET', 'POST']);
@@ -37,4 +33,4 @@ export default async function handler(req, res) {
     console.error('Internal Server Error:', error.message);
     res.status(500).json({ error: 'Internal Server Error' });
   }
-}
+};
