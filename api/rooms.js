70a4e44f250763d044cdaf84e9f421437c973aca
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-06-27 08:01:53 +0700

    Update rooms.js

--- api/rooms.js
+++ api/rooms.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   const SHEET_ID = '1a5dN1qvE2EgB7iUOv7MmioytlTLWTa81rgBRbPDfkj8';
   const SHEET_NAME = 'Sheet1';
-  const SHEET_RANGE = 'A2:J';
+  const SHEET_RANGE = 'A2:K';
   const sheetUrl = `https://docs.google.com/spreadsheets/d/${SHEET_ID}/gviz/tq?tqx=out:csv&sheet=${SHEET_NAME}&range=${SHEET_RANGE}`;
 
   try {
@@ -13,24 +13,22 @@ module.exports = async (req, res) => {
     }
     const text = await response.text();
     
-    // แปลง CSV เป็น Array และจัดการกับข้อมูล
     const rows = text.split('\n').map(row => {
-      // ใช้ regular expression เพื่อแยกฟิลด์ โดยคำนึงถึง comma ใน quotes
       const cells = row.match(/(".*?"|[^",\s]+)(?=\s*,|\s*$)/g);
       if (cells) {
-        // ลบ quotes ที่ไม่จำเป็นและ trim whitespace
         const cleanCells = cells.map(cell => cell.replace(/^"|"$/g, '').trim());
         return {
           roomNumber: cleanCells[0],
           price: cleanCells[1],
           status: cleanCells[2],
-          imageUrl: cleanCells[3].split(',').map(url => url.trim()), // แยก URL เป็น array
+          images: cleanCells[3].split(';').map(url => url.trim()),
           size: cleanCells[4],
-          amenities: cleanCells[5],
+          amenities: cleanCells[5].split(';').map(item => item.trim()),
           rentalStart: cleanCells[6],
           rentalEnd: cleanCells[7],
           latitude: cleanCells[8],
-          longitude: cleanCells[9]
+          longitude: cleanCells[9],
+          description: cleanCells[10]
         };
       }
       return null;
