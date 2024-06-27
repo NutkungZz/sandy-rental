43879f64bdd71deb42d3bd976e7921d6acbb1837
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-06-27 14:07:12 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,10 +1,16 @@
 module.exports = (req, res) => {
-  const { pathname } = new URL(req.url, 'https://${req.headers.host}');
+  const { pathname } = new URL(req.url, `http://${req.headers.host}`);
   
   if (pathname === '/api/rooms') {
     require('./rooms')(req, res);
-  } else if (pathname === '/api/test') {
-    res.json({ message: "API is working!" });
+  } else if (pathname === '/api/tenants') {
+    require('./tenants')(req, res);
+  } else if (pathname === '/api/login') {
+    require('./login')(req, res);
+  } else if (pathname.startsWith('/api/tenants/')) {
+    // สำหรับ PUT และ DELETE requests ที่มี id
+    req.query = { id: pathname.split('/')[3] };
+    require('./tenants')(req, res);
   } else {
     res.status(404).json({ error: "Not Found" });
   }
