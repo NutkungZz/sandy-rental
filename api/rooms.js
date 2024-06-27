ce058e4a3bad128700266fb285c7d4ae97ea0eb1
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-06-27 09:59:22 +0700

    Update rooms.js

--- api/rooms.js
+++ api/rooms.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'YOUR_SUPABASE_URL'
-const supabaseKey = 'YOUR_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://lqskrutydtcnnszvxaur.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxxc2tydXR5ZHRjbm5zenZ4YXVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTk0NTUzMDgsImV4cCI6MjAzNTAzMTMwOH0.9-LvOBprRoIdjnMpWATC7gIVfYa_RoJsgfNyEZegWbk'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default async function handler(req, res) {
