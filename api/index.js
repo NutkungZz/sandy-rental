4ad539afdee5c9f4a951ac9616502dea820c1a8e
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-06-28 09:07:14 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,18 +1,23 @@
+const bodyParser = require('body-parser');
+
 module.exports = (req, res) => {
-  const { pathname } = new URL(req.url, 'http://${req.headers.host}');
-  
-  if (pathname === '/api/rooms') {
-    require('./rooms')(req, res);
-  } else if (pathname === '/api/tenants') {
-    require('./tenants')(req, res);
-  } else if (pathname === '/api/login') {
-    require('./login')(req, res);
-  } else if (pathname === '/api/register') {
-    require('./register')(req, res);
-  } else if (pathname.startsWith('/api/tenants/')) {
-    req.query = { id: pathname.split('/')[3] };
-    require('./tenants')(req, res);
-  } else {
-    res.status(404).json({ error: "Not Found" });
-  }
+  // Parse JSON bodies
+  bodyParser.json()(req, res, () => {
+    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
+    
+    if (pathname === '/api/rooms') {
+      require('./rooms')(req, res);
+    } else if (pathname === '/api/tenants') {
+      require('./tenants')(req, res);
+    } else if (pathname === '/api/login') {
+      require('./login')(req, res);
+    } else if (pathname === '/api/register') {
+      require('./register')(req, res);
+    } else if (pathname.startsWith('/api/tenants/')) {
+      req.query = { id: pathname.split('/')[3] };
+      require('./tenants')(req, res);
+    } else {
+      res.status(404).json({ error: "Not Found" });
+    }
+  });
 };
