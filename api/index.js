079f1b95acdd18176dc87bae32231145326b5667
NutkungZz <131230214+NutkungZz@users.noreply.github.com>
2024-06-30 08:34:38 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,23 +3,29 @@ const bodyParser = require('body-parser');
 module.exports = (req, res) => {
   // Parse JSON bodies
   bodyParser.json()(req, res, () => {
-    const { pathname } = new URL(req.url, `http://${req.headers.host}`);
-    ///
-    if (pathname === '/api/rooms') {
+    const { pathname, searchParams } = new URL(req.url, `http://${req.headers.host}`);
+    const method = req.method;
+
+    if (pathname.startsWith('/api/rooms')) {
+      if (method === 'PUT' || method === 'DELETE') {
+        req.query = { id: pathname.split('/')[3] };
+      }
       require('./rooms')(req, res);
-    } else if (pathname === '/api/tenants') {
+    } else if (pathname.startsWith('/api/tenants')) {
+      if (method === 'PUT' || method === 'DELETE') {
+        req.query = { id: pathname.split('/')[3] };
+      }
       require('./tenants')(req, res);
+    } else if (pathname.startsWith('/api/payments')) {
+      if (method === 'PUT' || method === 'DELETE') {
+        req.query = { id: pathname.split('/')[3] };
+      }
+      require('./payments')(req, res);
     } else if (pathname === '/api/login') {
       require('./login')(req, res);
     } else if (pathname === '/api/register') {
       require('./register')(req, res);
-    } else if (pathname.startsWith('/api/tenants/')) {
-      req.query = { id: pathname.split('/')[3] };
-      require('./tenants')(req, res);
-    } else if (pathname === '/api/payments') {
-      require('./payments')(req, res);
-    } 
-    else {
+    } else {
       res.status(404).json({ error: "Not Found" });
     }
   });
